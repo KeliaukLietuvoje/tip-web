29f68e3a128ecc4d7d3b3fe625253ebd47142a27
LWangllix <vanglikas.lukas@gmail.com>
2024-08-13 15:01:33 +0300

    updates

---
+++ manifest.ts
@@ -0,0 +1,48 @@
+import { VitePWAOptions } from 'vite-plugin-pwa';
+
+export const manifestForPlugIn = (baseUrl = '') => {
+  return {
+    registerType: 'autoUpdate',
+    includeAssests: ['favicon.ico', 'apple-touc-icon.png', 'masked-icon.svg'],
+    workbox: {
+      navigateFallbackDenylist: [/^\/(api|files)\/.*/],
+    },
+    manifest: {
+      name: 'Turizmas',
+      short_name: 'Turizmas',
+      description: 'Turizmas',
+      icons: [
+        {
+          src: `${baseUrl}/android-chrome-192x192.png`,
+          sizes: '192x192',
+          type: 'image/png',
+          purpose: 'favicon',
+        },
+        {
+          src: `${baseUrl}/android-chrome-512x512.png`,
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'favicon',
+        },
+        {
+          src: `${baseUrl}/apple-touch-icon.png`,
+          sizes: '180x180',
+          type: 'image/png',
+          purpose: 'apple touch icon',
+        },
+        {
+          src: `${baseUrl}/maskable_icon.png`,
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable',
+        },
+      ],
+      theme_color: '#171717',
+      background_color: '#0a1353',
+      display: 'standalone',
+      start_url: `${baseUrl}/index.html`,
+      scope: `${baseUrl || '/'}`,
+      orientation: 'portrait',
+    },
+  };
+};
